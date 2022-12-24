9ebe1dc39dcf3892f899591e657b66fb93572d83
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2022-12-24 21:41:51 +0500

    Register Menu & Navigation regions

--- Inventory/Inventory/Inventory.Core/RegionNames.cs
+++ Inventory/Inventory/Inventory.Core/RegionNames.cs
@@ -3,5 +3,7 @@
     public static class RegionNames
     {
         public const string ContentRegion = "ContentRegion";
+        public const string MenuRegion = "MenuRegion";
+        public const string NavigationRegion = "NavigationRegion";
     }
 }
