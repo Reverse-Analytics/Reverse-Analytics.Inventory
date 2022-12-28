0ec4bdb0cd30134e416da76b5fe09eac6a00da07
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2022-12-28 22:55:32 +0500

    Add dashboard module

---
+++ Inventory/Inventory/Modules/Inventory.Modules.Dashboard/DashboardModule.cs
@@ -0,0 +1,19 @@
+﻿using Inventory.Modules.Dashboard.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Inventory.Modules.Dashboard
+{
+    public class DashboardModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<DashboardView>();
+        }
+    }
+}
---
+++ Inventory/Inventory/Modules/Inventory.Modules.Dashboard/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace Inventory.Modules.Dashboard.ViewModels
+{
+    public class DashboardViewModel : BindableBase
+    {
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        public DashboardViewModel()
+        {
+            Message = "View A from Dashboard";
+        }
+    }
+}
