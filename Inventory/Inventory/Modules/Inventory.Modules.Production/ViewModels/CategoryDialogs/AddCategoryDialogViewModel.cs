f3953783ba0e04f3b2c1ac8a912dc11c0f9e693c
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-05 23:54:06 +0500

    Implement initial AddCatalogVewModel

---
+++ Inventory/Inventory/Modules/Inventory.Modules.Production/ViewModels/CategoryDialogs/AddCategoryDialogViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Inventory.Core.Mvvm;
+using Prism.Commands;
+using Prism.Services.Dialogs;
+using System;
+
+namespace Inventory.Modules.Production.ViewModels.CategoryDialogs
+{
+    public class AddCategoryDialogViewModel : ViewModelBase, IDialogAware
+    {
+        public string Title => "Add Category";
+
+        public event Action<IDialogResult> RequestClose;
+        public DelegateCommand CloseDialogCommand { get; }
+
+        public AddCategoryDialogViewModel()
+        {
+            CloseDialogCommand = new DelegateCommand(CloseDialog);
+        }
+
+        private void CloseDialog()
+        {
+
+        }
+
+        public bool CanCloseDialog()
+        {
+            return false;
+        }
+
+        public void OnDialogClosed()
+        {
+            
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+            
+        }
+    }
+}
