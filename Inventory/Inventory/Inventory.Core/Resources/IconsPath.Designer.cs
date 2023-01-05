59fefb234c0084861c8c80a03517fcefdd81b325
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-05 23:44:36 +0500

    register sidebar icons in resources

--- Inventory/Inventory/Inventory.Core/Resources/IconsPath.Designer.cs
+++ Inventory/Inventory/Inventory.Core/Resources/IconsPath.Designer.cs
@@ -133,7 +133,7 @@ namespace Inventory.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to /Inventory.Core;Component/Resources/Icons/customers.ico.
+        ///   Looks up a localized string similar to M11.8784 9.9382C11.4873 9.54679 11.0303 9.22732 10.5284 8.99445C11.2362 8.42101 11.6878 7.54601 11.6878 6.56476C11.6878 4.83351 10.244 3.4132 8.51278 3.43976C6.8081 3.46632 5.43466 4.85538 5.43466 6.56476C5.43466 7.54601 5.88778 8.42101 6.59403 8.99445C6.09201 9.22715 5.635 9.54664 5.24403 9.9382C4.39091 10.7929 3.90653 11.921 3.87528 13.1241C3.87487 13.1408 3.87779 13.1574 3.88389 13.1729C3.88998 13.1885 3.89912 13.2026 3.91077 13.2146C3.92242 13.2265 3.93634 13.236 3.95171 13.2425C3.96709 13.2489 3.9836 1 [rest of string was truncated]&quot;;.
         /// </summary>
         public static string Customers {
             get {
@@ -142,7 +142,16 @@ namespace Inventory.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to /Inventory.Core;Component/Resources/Icons/dashboard.ico.
+        ///   Looks up a localized string similar to M11.8784 9.9382C11.4873 9.54679 11.0303 9.22732 10.5284 8.99445C11.2362 8.42101 11.6878 7.54601 11.6878 6.56476C11.6878 4.83351 10.244 3.4132 8.51278 3.43976C6.8081 3.46632 5.43466 4.85538 5.43466 6.56476C5.43466 7.54601 5.88778 8.42101 6.59403 8.99445C6.09201 9.22715 5.635 9.54664 5.24403 9.9382C4.39091 10.7929 3.90653 11.921 3.87528 13.1241C3.87487 13.1408 3.87779 13.1574 3.88389 13.1729C3.88998 13.1885 3.89912 13.2026 3.91077 13.2146C3.92242 13.2265 3.93634 13.236 3.95171 13.2425C3.96709 13.2489 3.9836 1 [rest of string was truncated]&quot;;.
+        /// </summary>
+        public static string CustomersIcon {
+            get {
+                return ResourceManager.GetString("CustomersIcon", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M12.4998 7.0923H6.90602V1.49855C6.90602 1.4298 6.84977 1.37355 6.78102 1.37355H6.37477C5.55398 1.37221 4.74104 1.53317 3.98269 1.84718C3.22435 2.16119 2.53558 2.62205 1.95602 3.20324C1.38525 3.77224 0.930468 4.44672 0.616953 5.18918C0.290711 5.95924 0.123304 6.78724 0.124765 7.62355C0.123421 8.44433 0.284384 9.25728 0.598393 10.0156C0.912403 10.774 1.37326 11.4627 1.95445 12.0423C2.52789 12.6157 3.19664 13.0673 3.94039 13.3814C4.71045 13.7076 5.53845 13.875 6.37477 13.8736C7.19555 13.8749 8.00849 13.7139 8. [rest of string was truncated]&quot;;.
         /// </summary>
         public static string Dashboard {
             get {
@@ -277,7 +286,7 @@ namespace Inventory.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to /Inventory.Core;Component/Resources/Icons/inventory.ico.
+        ///   Looks up a localized string similar to M11.9996 0H1.99963C1.72307 0 1.49963 0.223437 1.49963 0.5V13.5C1.49963 13.7766 1.72307 14 1.99963 14H11.9996C12.2762 14 12.4996 13.7766 12.4996 13.5V0.5C12.4996 0.223437 12.2762 0 11.9996 0ZM11.3746 12.875H2.62463V9.73438H4.15432C4.33557 10.2469 4.65432 10.7078 5.07776 11.0578C5.61682 11.5031 6.29963 11.75 6.99963 11.75C7.69963 11.75 8.38245 11.5047 8.92151 11.0578C9.34495 10.7078 9.6637 10.2469 9.84495 9.73438H11.3746V8.75H9.05588L8.97464 9.13594C8.78714 10.0703 7.95588 10.75 6.99963 10.75C6.04338 10.75 5. [rest of string was truncated]&quot;;.
         /// </summary>
         public static string Inventory {
             get {
@@ -304,7 +313,7 @@ namespace Inventory.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to /Inventory.Core;Component/Resources/Icons/production.ico.
+        ///   Looks up a localized string similar to M12.7815 3.25345V1.25189C12.7815 0.97533 12.5581 0.751892 12.2815 0.751892H1.719C1.44244 0.751892 1.219 0.97533 1.219 1.25189V3.25345C0.958066 3.26908 0.750254 3.48627 0.750254 3.75189V5.80971C0.749253 6.1879 0.825831 6.56228 0.975254 6.90971C1.04244 7.06908 1.12525 7.21908 1.219 7.36127V12.7519C1.219 13.0269 1.44244 13.2519 1.719 13.2519H12.2815C12.5581 13.2519 12.7815 13.0285 12.7815 12.7519V7.36127C12.8767 7.21872 12.9583 7.06753 13.0253 6.90971C13.1737 6.56127 13.2503 6.19096 13.2503 5.80971V3.75189C13. [rest of string was truncated]&quot;;.
         /// </summary>
         public static string Production {
             get {
@@ -313,7 +322,7 @@ namespace Inventory.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to /Inventory.Core;Component/Resources/Icons/reports.ico.
+        ///   Looks up a localized string similar to M12.8748 11.3755H2.12476V1.62549C2.12476 1.55674 2.06851 1.50049 1.99976 1.50049H1.12476C1.05601 1.50049 0.999756 1.55674 0.999756 1.62549V12.3755C0.999756 12.4442 1.05601 12.5005 1.12476 12.5005H12.8748C12.9435 12.5005 12.9998 12.4442 12.9998 12.3755V11.5005C12.9998 11.4317 12.9435 11.3755 12.8748 11.3755ZM3.77788 8.96455C3.82632 9.01299 3.90444 9.01299 3.95444 8.96455L6.11538 6.81455L8.10913 8.8208C8.15757 8.86924 8.23726 8.86924 8.2857 8.8208L12.5888 4.51924C12.6373 4.4708 12.6373 4.39111 12.5888 4.34268 [rest of string was truncated]&quot;;.
         /// </summary>
         public static string Reports {
             get {
@@ -331,7 +340,7 @@ namespace Inventory.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to /Inventory.Core;Component/Resources/Icons/sale.ico.
+        ///   Looks up a localized string similar to M7 0C3.13438 0 0 3.13438 0 7C0 10.8656 3.13438 14 7 14C10.8656 14 14 10.8656 14 7C14 3.13438 10.8656 0 7 0ZM7 12.8125C3.79063 12.8125 1.1875 10.2094 1.1875 7C1.1875 3.79063 3.79063 1.1875 7 1.1875C10.2094 1.1875 12.8125 3.79063 12.8125 7C12.8125 10.2094 10.2094 12.8125 7 12.8125ZM7.74531 6.6375L7.34844 6.54531V4.44688C7.94219 4.52813 8.30937 4.9 8.37187 5.35625C8.37969 5.41875 8.43281 5.46406 8.49531 5.46406H9.19687C9.27031 5.46406 9.32812 5.4 9.32187 5.32656C9.22656 4.35313 8.425 3.72813 7.35469 3.62031V3. [rest of string was truncated]&quot;;.
         /// </summary>
         public static string Sales {
             get {
@@ -340,7 +349,16 @@ namespace Inventory.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to /Inventory.Core;Component/Resources/Icons/settings.ico.
+        ///   Looks up a localized string similar to M15.102 14.1188L10.4645 9.48129C11.1841 8.55093 11.5734 7.41343 11.5734 6.21701C11.5734 4.78486 11.0145 3.44201 10.0038 2.42951C8.99306 1.41701 7.64663 0.859863 6.21627 0.859863C4.78592 0.859863 3.43949 1.41879 2.42877 2.42951C1.41627 3.44022 0.859131 4.78486 0.859131 6.21701C0.859131 7.64736 1.41806 8.99379 2.42877 10.0045C3.43949 11.017 4.78413 11.5741 6.21627 11.5741C7.4127 11.5741 8.54842 11.1849 9.47877 10.467L14.1163 15.1027C14.1299 15.1163 14.146 15.1271 14.1638 15.1345C14.1816 15.1418 14.2006 15.145 [rest of string was truncated]&quot;;.
+        /// </summary>
+        public static string Search {
+            get {
+                return ResourceManager.GetString("Search", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M13.4498 8.77888L12.4264 7.90388C12.4748 7.607 12.4998 7.30388 12.4998 7.00075C12.4998 6.69763 12.4748 6.3945 12.4264 6.09763L13.4498 5.22263C13.527 5.15654 13.5823 5.06853 13.6082 4.97028C13.6342 4.87203 13.6296 4.76821 13.5951 4.67263L13.5811 4.632C13.2993 3.84454 12.8774 3.11456 12.3358 2.47731L12.3076 2.4445C12.2419 2.36724 12.1544 2.3117 12.0565 2.2852C11.9586 2.2587 11.8549 2.26249 11.7592 2.29606L10.4889 2.74763C10.0201 2.36325 9.49671 2.06013 8.93108 1.84763L8.68577 0.519501C8.66726 0.419567 8.61878 [rest of string was truncated]&quot;;.
         /// </summary>
         public static string Settings {
             get {
@@ -349,7 +367,7 @@ namespace Inventory.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to /Inventory.Core;Component/Resources/Icons/suppliers.ico.
+        ///   Looks up a localized string similar to M12.9376 11.061H11.6876V9.81098C11.6876 9.74223 11.6313 9.68598 11.5626 9.68598H10.8126C10.7438 9.68598 10.6876 9.74223 10.6876 9.81098V11.061H9.43756C9.36881 11.061 9.31256 11.1172 9.31256 11.186V11.936C9.31256 12.0047 9.36881 12.061 9.43756 12.061H10.6876V13.311C10.6876 13.3797 10.7438 13.436 10.8126 13.436H11.5626C11.6313 13.436 11.6876 13.3797 11.6876 13.311V12.061H12.9376C13.0063 12.061 13.0626 12.0047 13.0626 11.936V11.186C13.0626 11.1172 13.0063 11.061 12.9376 11.061ZM4.836 6.78598C4.82194 6.65004 4. [rest of string was truncated]&quot;;.
         /// </summary>
         public static string Suppliers {
             get {
@@ -358,7 +376,7 @@ namespace Inventory.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to /Inventory.Core;Component/Resources/Icons/supplies.ico.
+        ///   Looks up a localized string similar to M1.90751 4.05999L7.00001 7.00583L12.0925 4.05999M7 12.88V6.99999M12.25 9.33332V4.66665C12.2498 4.46206 12.1958 4.26112 12.0934 4.084C11.991 3.90687 11.8438 3.75978 11.6667 3.65748L7.58333 1.32415C7.40598 1.22175 7.20479 1.16785 7 1.16785C6.79521 1.16785 6.59402 1.22175 6.41667 1.32415L2.33333 3.65748C2.15615 3.75978 2.00899 3.90687 1.9066 4.084C1.80422 4.26112 1.75021 4.46206 1.75 4.66665V9.33332C1.75021 9.53791 1.80422 9.73885 1.9066 9.91597C2.00899 10.0931 2.15615 10.2402 2.33333 10.3425L6.41667 12.6758C6 [rest of string was truncated]&quot;;.
         /// </summary>
         public static string Supplies {
             get {
