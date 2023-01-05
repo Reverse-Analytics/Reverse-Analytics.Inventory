a5d22ab1e93ba4aecc89e1fdcf7d74487b4a9a7a
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-05 23:48:14 +0500

    Add dialog names for categories view

--- Inventory/Inventory/Inventory.Core/ViewNames.cs
+++ Inventory/Inventory/Inventory.Core/ViewNames.cs
@@ -6,4 +6,11 @@
         public const string Catalog = "CatalogView";
         public const string Categories = "CategoriesView";
     }
+
+    public static class CategoryDialogNames
+    {
+        public const string AddCategory = "AddCategoryDialogView";
+        public const string UpdateCategory = "UpdateCategoryDialogView";
+        public const string CategoryDetail = "CategoryDetailsDialogView";
+    }
 }
