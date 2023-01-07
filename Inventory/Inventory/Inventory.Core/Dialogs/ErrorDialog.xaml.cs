d97ba0d167f34949736bafef2413cf5dcde0e9c2
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-07 22:25:04 +0500

    Implement error dialog

---
+++ Inventory/Inventory/Inventory.Core/Dialogs/ErrorDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace Inventory.Core.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ErrorDialog.xaml
+    /// </summary>
+    public partial class ErrorDialog : UserControl
+    {
+        public ErrorDialog()
+        {
+            InitializeComponent();
+        }
+
+        public ErrorDialog(string message)
+            : this()
+        {
+            this.description.Text = message;
+        }
+
+        public ErrorDialog(string title, string message)
+            : this(message)
+        {
+            this.title.Text = title;
+        }
+    }
+}
