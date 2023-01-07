0ce65b09456f0d56230ba24c23b008cc66f3a7f4
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-07 22:25:19 +0500

    Implement generic confirmation dialog

---
+++ Inventory/Inventory/Inventory.Core/Dialogs/ConfirmationDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace Inventory.Core.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ConfirmationDialog.xaml
+    /// </summary>
+    public partial class ConfirmationDialog : UserControl
+    {
+        public ConfirmationDialog()
+        {
+            InitializeComponent();
+        }
+
+        public ConfirmationDialog(string message)
+            : this()
+        {
+            this.description.Text = message;
+        }
+
+        public ConfirmationDialog(string title, string message)
+            : this(message)
+        {
+            this.title.Text = title;
+        }
+    }
+}
