d6a40d45036e76584dcbb50c76dbe3d0269830b8
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-07 22:27:26 +0500

    Add separate dialogs module

---
+++ Inventory/Inventory/Modules/Inventory.Modules.Dialogs/DialogsModule.cs
@@ -0,0 +1,20 @@
+﻿using Inventory.Modules.Dialogs.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Inventory.Modules.Dialogs
+{
+    public class DialogsModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ConfirmationDialog>();
+        }
+    }
+}
