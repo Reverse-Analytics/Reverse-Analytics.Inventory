9fe8cd5bffb89cebaeddf4586b568b93fd6f3ece
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-07 22:28:20 +0500

    add dialog service interface

---
+++ Inventory/Inventory/Services/Inventory.Services.Interfaces/IDialogService.cs
@@ -0,0 +1,13 @@
+﻿namespace Inventory.Services.Interfaces
+{
+    public interface IDialogService
+    {
+        public void ShowError();
+        public void ShowError(string message);
+        public void ShowError(string title, string message);
+        public void ShowError(string title, string message, Exception exception);
+        public Task<bool> ShowConfirmation();
+        public Task<bool> ShowConfirmation(string title);
+        public Task<bool> ShowConfirmation(string title, string message);
+    }
+}
