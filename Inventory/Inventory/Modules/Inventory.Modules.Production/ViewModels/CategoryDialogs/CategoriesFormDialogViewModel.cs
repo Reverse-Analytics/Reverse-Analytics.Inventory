320e622e135488ae4a8917edabed62ec936ac553
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-07 22:31:54 +0500

    Implement generic form for Categories

---
+++ Inventory/Inventory/Modules/Inventory.Modules.Production/ViewModels/CategoryDialogs/CategoriesFormDialogViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Inventory.Core.Mvvm;
+using MaterialDesignThemes.Wpf;
+using Prism.Commands;
+using System.Diagnostics;
+
+namespace Inventory.Modules.Production.ViewModels.CategoryDialogs
+{
+    public class CategoriesFormDialogViewModel : ViewModelBase
+    {
+        private string _title = "Add Category";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private string _categoryName;
+        public string CategoryName 
+        {
+            get => _categoryName;
+            set
+            {
+                SetProperty(ref _categoryName, value);
+                SaveCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public bool IsCategoryNameInvalid => CanSave();
+
+        public DelegateCommand SaveCommand { get; }
+        public DelegateCommand CancelCommand { get; }
+        public DialogClosingEventHandler ClosingCommand { get; }
+
+        public CategoriesFormDialogViewModel()
+        {
+            SaveCommand = new DelegateCommand(OnSave, CanSave);
+            CancelCommand = new DelegateCommand(OnCancel);
+            ClosingCommand = ClosingEventHandler;
+        }
+
+        private void ClosingEventHandler(object sender, DialogClosingEventArgs eventArgs)
+            => Debug.WriteLine("You can intercept the closing event, and cancel here.");
+
+        private void OnSave()
+        {
+            DialogHost.Close("RootDialog", CategoryName);
+        }
+
+        private bool CanSave()
+        {
+            return !string.IsNullOrEmpty(CategoryName);
+        }
+
+        private void OnCancel()
+        {
+            DialogHost.Close("RootDialog");
+        }
+
+    }
+}
---
+++ Inventory/Inventory/Modules/Inventory.Modules.Production/Views/CategoryDialogs/CategoriesFormDialogView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inventory.Modules.Production.Views.CategoryDialogs
+{
+    /// <summary>
+    /// Interaction logic for AddCategoryDialogView.xaml
+    /// </summary>
+    public partial class CategoriesFormDialogView : UserControl
+    {
+        public CategoriesFormDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
