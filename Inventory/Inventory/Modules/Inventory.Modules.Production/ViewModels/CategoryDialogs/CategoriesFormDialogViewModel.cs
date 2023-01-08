d55323461826045de7c133915eccf21b2ec17805
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-08 20:54:58 +0500

    Update CategoriesForm to serve as a editing and adding form

--- Inventory/Inventory/Modules/Inventory.Modules.Production/ViewModels/CategoryDialogs/CategoriesFormDialogViewModel.cs
+++ Inventory/Inventory/Modules/Inventory.Modules.Production/ViewModels/CategoryDialogs/CategoriesFormDialogViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using Inventory.Core.Mvvm;
 using MaterialDesignThemes.Wpf;
 using Prism.Commands;
+using ReverseAnalytics.Domain.DTOs.ProductCategory;
 using System.Diagnostics;
 
 namespace Inventory.Modules.Production.ViewModels.CategoryDialogs
 {
     public class CategoriesFormDialogViewModel : ViewModelBase
     {
+        private readonly bool isEditingMode;
+        private readonly int id;
+
         private string _title = "Add Category";
         public string Title
         {
@@ -38,12 +42,29 @@ namespace Inventory.Modules.Production.ViewModels.CategoryDialogs
             ClosingCommand = ClosingEventHandler;
         }
 
+        public CategoriesFormDialogViewModel(ProductCategoryDto categoryToUpdate)
+            : this()
+        {
+            CategoryName = categoryToUpdate.CategoryName;
+            id = categoryToUpdate.Id;
+            isEditingMode = true;
+        }
+
         private void ClosingEventHandler(object sender, DialogClosingEventArgs eventArgs)
             => Debug.WriteLine("You can intercept the closing event, and cancel here.");
 
         private void OnSave()
         {
-            DialogHost.Close("RootDialog", CategoryName);
+            if (isEditingMode)
+            {
+                var category = new ProductCategoryForUpdateDto(id, CategoryName);
+                DialogHost.Close("RootDialog", category);
+            }
+            else
+            {
+                var category = new ProductCategoryForCreateDto(CategoryName);
+                DialogHost.Close("RootDialog", category);
+            }
         }
 
         private bool CanSave()
