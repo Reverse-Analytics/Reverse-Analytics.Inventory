d1d241ba06f65fc9079245f8eb6434478611098b
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-08 20:54:10 +0500

    Implement rest client for Reverse analytics

---
+++ Inventory/Inventory/Services/Inventory.RestClient/RestClientBase.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+namespace Inventory.RestClient
+{
+    public class RestClientBase
+    {
+        private const string urlBase = "https://localhost:7231/api";
+        private readonly HttpClient client;
+
+        public RestClientBase()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(urlBase);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<HttpResponseMessage?> Get(string url)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{client.BaseAddress?.AbsoluteUri}/{url}");
+            var response = await client.SendAsync(request);
+
+            return response;
+        }
+
+        public async Task<HttpResponseMessage?> Post(string url, string json)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, $"{client.BaseAddress?.AbsoluteUri}/{url}");
+            request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.SendAsync(request);
+
+            return response;
+        }
+
+        public async Task<HttpResponseMessage?> Put(string url, string json)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Put, $"{client.BaseAddress?.AbsoluteUri}/{url}");
+            request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.SendAsync(request);
+
+            return response;
+        }
+
+        public async Task<HttpResponseMessage?> Delete(string url)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, $"{client.BaseAddress?.AbsoluteUri}/{url}");
+            var response = await client.SendAsync(request);
+
+            return response;
+        }
+    }
+}
