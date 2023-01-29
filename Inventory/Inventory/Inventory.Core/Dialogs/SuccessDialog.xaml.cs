7d439d6f283510147003cb17c1d959e7261a7600
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-29 22:56:22 +0500

    Improve success dialog

---
+++ Inventory/Inventory/Inventory.Core/Dialogs/SuccessDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+
+namespace Inventory.Core.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for SuccessDialog.xaml
+    /// </summary>
+    public partial class SuccessDialog : UserControl
+    {
+        public SuccessDialog()
+        {
+            InitializeComponent();
+        }
+
+        public SuccessDialog(string message)
+            : this()
+        {
+            if (!string.IsNullOrEmpty(message))
+                this.message.Text = message;
+        }
+
+        public SuccessDialog(string title, string message)
+            : this(message)
+        {
+            if (!string.IsNullOrEmpty(title))
+                this.title.Text = title;
+        }
+    }
+}
