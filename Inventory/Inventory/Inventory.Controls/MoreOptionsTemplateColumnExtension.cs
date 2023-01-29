d9953cc6e8123ff07322492435dc5cfc7331f424
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-29 16:10:15 +0500

    Implement custom template column extension

---
+++ Inventory/Inventory/Inventory.Controls/MoreOptionsTemplateColumnExtension.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.UI.Xaml.Grid.Cells;
+using System.Windows;
+
+namespace Inventory.Controls
+{
+    // Create Grid template column extension and specify it's cell type
+    public class MoreOptionsTemplateColumnExtension : GridTemplateColumn
+    {
+        public MoreOptionsTemplateColumnExtension()
+        {
+            SetCellType("TemplateExt");
+        }
+    }
+
+    public class GridCellTemplateExtension : GridCellTemplateRenderer
+    {
+        // This method will set the focus to the clicked cell
+        // instead of row itself
+        protected override void SetFocus(FrameworkElement uiElement, bool needToFocus)
+        {
+            if (!needToFocus)
+                DataGrid.Focus();
+        }
+    }
+}
