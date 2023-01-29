de29bbc9634eb27f6c75f8b004914f3d30d4da9f
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-29 16:19:33 +0500

    Improve code, fix popup lost focus issue using custom control extension

--- Inventory/Inventory/Modules/Inventory.Modules.Production/Views/ProductsView.xaml.cs
+++ Inventory/Inventory/Modules/Inventory.Modules.Production/Views/ProductsView.xaml.cs
@@ -1,6 +1,4 @@
-﻿using MaterialDesignThemes.Wpf;
-using Syncfusion.UI.Xaml.Grid;
-using System.Windows;
+﻿using Inventory.Controls;
 using System.Windows.Controls;
 
 namespace Inventory.Modules.Production.Views
@@ -10,28 +8,12 @@ namespace Inventory.Modules.Production.Views
     /// </summary>
     public partial class ProductsView : UserControl
     {
-        PopupBox selectedPopupBox;
         public ProductsView()
         {
             InitializeComponent();
-        }
 
-        private void PopupBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            var popupBox = sender as PopupBox;
-
-            selectedPopupBox = popupBox;
-
-            popupBox.IsPopupOpen = true;
-        }
-
-        private void ProductsDataGrid_SelectionChanged(object sender, GridSelectionChangedEventArgs e)
-        {
-            if (selectedPopupBox != null)
-            {
-                selectedPopupBox.Focus();
-                selectedPopupBox.IsPopupOpen = true;
-            }
+            productsDataGrid.CellRenderers.Remove("TemplateExt");
+            productsDataGrid.CellRenderers.Add("TemplateExt", new GridCellTemplateExtension());
         }
     }
 }
