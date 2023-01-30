140c17ca789c61730cbd1e50f2e72e4a36b8dc1e
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-01-30 23:44:07 +0500

    Update dialogs

---
+++ Inventory/Inventory/Inventory.Core/Dialogs/SuccessDialogViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Services.Dialogs;
+using System;
+
+namespace Inventory.Core.Dialogs
+{
+    public class SuccessDialogViewModel : IDialogAware
+    {
+        public string Title => "Success";
+
+        public event Action<IDialogResult> RequestClose;
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+
+        }
+    }
+}
