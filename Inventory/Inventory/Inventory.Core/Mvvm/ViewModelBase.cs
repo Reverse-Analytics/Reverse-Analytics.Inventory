9512f351416a06de91ac16a4b474b148be371dcb
Miraziz <khidoyatov1997@gmail.com>
2023-02-01 22:03:53 +0500

    Remove calling dialogs from base VM

--- Inventory/Inventory/Inventory.Core/Mvvm/ViewModelBase.cs
+++ Inventory/Inventory/Inventory.Core/Mvvm/ViewModelBase.cs
@@ -2,7 +2,6 @@
 using MaterialDesignThemes.Wpf;
 using Prism.Mvvm;
 using Prism.Navigation;
-using Prism.Services.Dialogs;
 
 namespace Inventory.Core.Mvvm
 {
@@ -18,18 +17,20 @@ namespace Inventory.Core.Mvvm
 
                 if (value)
                 {
+                    var session = DialogHost.GetDialogSession(RegionNames.ProgressRegion);
+                    session?.Close();
+
                     var progressDialog = new ProgressDialog();
 
-                    DialogHost.Show(progressDialog, RegionNames.NotificationRegion).ConfigureAwait(false);
+                    DialogHost.Show(progressDialog, RegionNames.ProgressRegion).ConfigureAwait(false);
                 }
                 else
                 {
-                    DialogHost.Close(RegionNames.NotificationRegion);
+                    var session = DialogHost.GetDialogSession(RegionNames.ProgressRegion);
+                    session?.Close();
                 }
             }
         }
-
-        private readonly IDialogService _dialogService;
 
         protected ViewModelBase()
         {
@@ -38,48 +39,6 @@ namespace Inventory.Core.Mvvm
         public virtual void Destroy()
         {
 
-        }
-
-        public static async void ShowSuccessDialog()
-        {
-            var successDialog = new SuccessDialog();
-
-            await DialogHost.Show(successDialog, RegionNames.NotificationRegion);
-        }
-
-        public static async void ShowSuccessDialog(string message)
-        {
-            var successDialog = new SuccessDialog(message: message);
-
-            await DialogHost.Show(successDialog, RegionNames.NotificationRegion);
-        }
-
-        public static async void ShowSuccessDialog(string title, string message)
-        {
-            var successDialog = new SuccessDialog(title: title, message: message);
-
-            await DialogHost.Show(successDialog, RegionNames.NotificationRegion);
-        }
-
-        public static async void ShowErrorDialog()
-        {
-            var errorDialog = new ErrorDialog();
-
-            await DialogHost.Show(errorDialog, RegionNames.NotificationRegion);
-        }
-
-        public static async void ShowErrorDialog(string message)
-        {
-            var errorDialog = new ErrorDialog(message: message);
-
-            await DialogHost.Show(errorDialog, RegionNames.NotificationRegion);
-        }
-
-        public static async void ShowErrorDialog(string title, string message)
-        {
-            var errorDialog = new ErrorDialog(title: title, message: message);
-
-            await DialogHost.Show(errorDialog, RegionNames.NotificationRegion);
         }
     }
 }
