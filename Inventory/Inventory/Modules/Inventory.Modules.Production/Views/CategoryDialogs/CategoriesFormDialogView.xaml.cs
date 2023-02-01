eb2eb025018c07e941a15a1cf3b88a303115b219
Miraziz <khidoyatov1997@gmail.com>
2023-02-01 22:04:57 +0500

    Handle key inputs from user

--- Inventory/Inventory/Modules/Inventory.Modules.Production/Views/CategoryDialogs/CategoriesFormDialogView.xaml.cs
+++ Inventory/Inventory/Modules/Inventory.Modules.Production/Views/CategoryDialogs/CategoriesFormDialogView.xaml.cs
@@ -1,18 +1,6 @@
-﻿using Prism.Services.Dialogs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Inventory.Modules.Production.ViewModels.CategoryDialogs;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Inventory.Modules.Production.Views.CategoryDialogs
 {
@@ -24,6 +12,22 @@ namespace Inventory.Modules.Production.Views.CategoryDialogs
         public CategoriesFormDialogView()
         {
             InitializeComponent();
+
+            PreviewKeyDown += new KeyEventHandler(HandleKeyPress);
+        }
+
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            var vm = DataContext as CategoriesFormDialogViewModel;
+
+            if (e.Key == Key.Escape)
+            {
+                vm?.CloseCommand?.Execute();
+            }
+            else if (e.Key == Key.Enter && (vm?.SaveCommand?.CanExecute() ?? false))
+            {
+                vm.SaveCommand.Execute();
+            }
         }
     }
 }
