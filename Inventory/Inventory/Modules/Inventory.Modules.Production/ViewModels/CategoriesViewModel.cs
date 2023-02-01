c24c4dd1654ab13a7c5ebb69924679697fed7676
Miraziz <khidoyatov1997@gmail.com>
2023-02-01 22:05:38 +0500

    refactor categories VM

--- Inventory/Inventory/Modules/Inventory.Modules.Production/ViewModels/CategoriesViewModel.cs
+++ Inventory/Inventory/Modules/Inventory.Modules.Production/ViewModels/CategoriesViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using Inventory.Core;
-using Inventory.Core.Dialogs;
 using Inventory.Core.Mvvm;
 using Inventory.Modules.Production.ViewModels.CategoryDialogs;
 using Inventory.Modules.Production.ViewModels.Forms;
@@ -21,19 +20,34 @@ namespace Inventory.Modules.Production.ViewModels
 {
     public class CategoriesViewModel : ViewModelBase, IRegionMemberLifetime
     {
+        #region Services
+
+        private readonly IDialogService _dialogService;
         private readonly ICategorySerivce _categoryService;
 
-        private string _title;
+        #endregion
+
+        #region Properties
+
+        private string _title = "Categories";
         public string Title
         {
             get => _title;
             set => SetProperty(ref _title, value);
         }
 
+        #endregion
+
         public bool KeepAlive => false;
+
+        #region Collections
 
         private List<ProductCategoryDto> _categoriesList;
         public ObservableCollection<ProductCategoryDto> Categories { get; set; }
+
+        #endregion
+
+        #region Commands
 
         public ICommand AddCommand { get; }
         public ICommand UpdateCommand { get; }
@@ -41,14 +55,14 @@ namespace Inventory.Modules.Production.ViewModels
         public ICommand ArchiveCommand { get; }
         public ICommand ShowDetailsCommand { get; }
 
-        private readonly Prism.Services.Dialogs.IDialogService _dialogService;
+        #endregion
 
-        public CategoriesViewModel(ICategorySerivce categorySerivce, Prism.Services.Dialogs.IDialogService dialogService)
+        public CategoriesViewModel(ICategorySerivce categorySerivce, IDialogService dialogService)
         {
-            Title = "Categories";
             Categories = new ObservableCollection<ProductCategoryDto>();
 
             _categoryService = categorySerivce;
+            _dialogService = dialogService;
 
             AddCommand = new DelegateCommand(OnAddCategory);
             UpdateCommand = new DelegateCommand<ProductCategoryDto>(OnUpdate);
@@ -57,34 +71,38 @@ namespace Inventory.Modules.Production.ViewModels
             ShowDetailsCommand = new DelegateCommand<ProductCategoryDto>(OnShowDetails);
 
             LoadCategories();
-            _dialogService = dialogService;
         }
 
         private async void LoadCategories()
         {
-            var categories = await _categoryService.GetCategoriesAsync();
-            _categoriesList = new List<ProductCategoryDto>(categories);
+            try
+            {
+                var categories = await _categoryService.GetCategoriesAsync();
+                _categoriesList = new List<ProductCategoryDto>(categories);
 
-            Categories.AddRange(categories);
+                Categories.AddRange(categories);
+            }
+            catch (Exception ex)
+            {
+                await _dialogService.ShowError(message: "There was an error retrieving categories... Please, try again later.");
+                Debug.WriteLine(ex);
+            }
         }
+
+        #region Command methods
 
         private async void OnAddCategory()
         {
             try
             {
-                var view = new CategoriesFormDialogView()
-                {
-                    DataContext = new CategoriesFormDialogViewModel()
-                };
+                var category = await ShowAddCategoryForm();
 
-                var result = await DialogHost.Show(view, RegionNames.DialogRegion);
-
-                if (result is null || result is not ProductCategoryForCreateDto)
+                if (category is null)
                 {
                     return;
                 }
 
-                var category = result as ProductCategoryForCreateDto;
+                IsBusy = true;
 
                 await Task.Run(async () =>
                 {
@@ -96,70 +114,74 @@ namespace Inventory.Modules.Production.ViewModels
                     }
                 });
 
-                ShowSuccessDialog();
+                await _dialogService.ShowSuccess(message: $"Category: {category.CategoryName} was added.");
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex);
 
-                ShowErrorDialog();
+                await _dialogService.ShowError(message: $"There was an error adding new Category. Please, try again later.");
+            }
+            finally
+            {
+                IsBusy = false;
             }
         }
 
         private async void OnUpdate(ProductCategoryDto selectedCategory)
         {
-            var view = new CategoriesFormDialogView()
-            {
-                DataContext = new CategoriesFormDialogViewModel(selectedCategory)
-            };
-
-            var result = await DialogHost.Show(view, RegionNames.DialogRegion);
-
-            if (result is null || result is not ProductCategoryForUpdateDto)
-            {
-                return;
-            }
-
-            var category = result as ProductCategoryForUpdateDto;
-
             try
             {
+                var category = await ShowUpdateCategoryForm(selectedCategory);
+
+                if (category is null)
+                {
+                    return;
+                }
+
+                IsBusy = true;
+
                 await Task.Run(async () =>
                 {
                     await _categoryService.UpdateCategoryAsync(category);
                 });
 
                 selectedCategory.CategoryName = category.CategoryName;
+
                 Categories.Clear();
                 Categories.AddRange(_categoriesList);
 
-                ShowSuccessDialog($"Category: {category.CategoryName} was successfully updated.");
+                await _dialogService.ShowSuccess(message: $"Category: {category.CategoryName} was successfully updated.");
             }
             catch (Exception ex)
             {
-                ShowErrorDialog("There was an error while updating category. Please, try again later");
+                await _dialogService.ShowError(message: "There was an error while updating category. Please, try again later");
                 Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
             }
         }
 
         private async void OnDelete(ProductCategoryDto selectedCategory)
         {
-            if (selectedCategory == null)
-            {
-                return;
-            }
-
-            var view = new ConfirmationDialog("Confirm action.", $"Are you sure you want to delete: {selectedCategory.CategoryName}?");
-
-            var result = await DialogHost.Show(view, RegionNames.DialogRegion);
-
-            if (result is null || result is false)
-            {
-                return;
-            }
-
             try
             {
+                if (selectedCategory == null)
+                {
+                    return;
+                }
+
+                var isConfirm = await _dialogService.ShowConfirmation();
+
+                if (!isConfirm)
+                {
+                    return;
+                }
+
+                IsBusy = true;
+
                 await Task.Run(async () =>
                 {
                     await _categoryService.DeleteCategoryAsync(selectedCategory.Id);
@@ -167,38 +189,51 @@ namespace Inventory.Modules.Production.ViewModels
                     Categories.Remove(selectedCategory);
                 });
 
-                ShowSuccessDialog($"Category: {selectedCategory.CategoryName} was successfully deleted.");
+                await _dialogService.ShowSuccess(message: $"Category: {selectedCategory.CategoryName} was successfully deleted.");
             }
             catch (Exception ex)
             {
-                ShowErrorDialog("There was an error while deleting category. Please, try again later.");
+                await _dialogService.ShowError(message: "There was an error while deleting category. Please, try again later.");
                 Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
             }
         }
 
         private async void OnArchive(ProductCategoryDto selectedCategory)
         {
-
-
-            if (selectedCategory == null)
+            try
             {
-                return;
+                if (selectedCategory == null)
+                {
+                    return;
+                }
+
+                var isConfirm = await _dialogService.ShowConfirmation();
+
+                if (!isConfirm)
+                {
+                    return;
+                }
+
+                IsBusy = true;
+
+                // TODO implement archiving
+                await Task.Delay(1500);
+
+                await _dialogService.ShowSuccess(message: $"Category: {selectedCategory.CategoryName} was successfully archived.");
             }
-
-            var view = new ConfirmationDialog("Confirm action.", $"Are you sure you want to archive: {selectedCategory.CategoryName}?");
-
-            var result = await DialogHost.Show(view, RegionNames.DialogRegion);
-
-            if (result is null || result is false)
+            catch (Exception ex)
             {
-                return;
+                await _dialogService.ShowError(message: $"There was an error archiving Category: {selectedCategory.CategoryName}. Please, try again later");
+                Debug.WriteLine(ex);
             }
-
-            // TODO implement archive
-
-            await Task.Delay(1000);
-
-            ShowSuccessDialog($"Category: {selectedCategory.CategoryName} was successfully archived.");
+            finally
+            {
+                IsBusy = false;
+            }
         }
 
         private async void OnShowDetails(ProductCategoryDto selectedCategory)
@@ -210,5 +245,50 @@ namespace Inventory.Modules.Production.ViewModels
 
             await DialogHost.Show(view, RegionNames.DialogRegion);
         }
+
+        #endregion
+
+        #region Helper methods
+
+        private static async Task<ProductCategoryForCreateDto> ShowAddCategoryForm()
+        {
+            var view = new CategoriesFormDialogView()
+            {
+                DataContext = new CategoriesFormDialogViewModel()
+            };
+
+            var result = await DialogHost.Show(view, RegionNames.DialogRegion);
+
+            if (result is null || result is not ProductCategoryForCreateDto category)
+            {
+                return null;
+            }
+
+            return category;
+        }
+
+        private static async Task<ProductCategoryForUpdateDto> ShowUpdateCategoryForm(ProductCategoryDto categoryToUpdate)
+        {
+            if (categoryToUpdate is null)
+            {
+                return null;
+            }
+
+            var view = new CategoriesFormDialogView()
+            {
+                DataContext = new CategoriesFormDialogViewModel(categoryToUpdate)
+            };
+
+            var result = await DialogHost.Show(view, RegionNames.DialogRegion);
+
+            if (result is null || result is not ProductCategoryForUpdateDto updatedCategory)
+            {
+                return null;
+            }
+
+            return updatedCategory;
+        }
+
+        #endregion
     }
 }
