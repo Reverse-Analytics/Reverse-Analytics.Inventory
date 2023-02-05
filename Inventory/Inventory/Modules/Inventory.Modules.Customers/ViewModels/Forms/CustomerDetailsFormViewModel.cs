d50b1448f03c0e4efdae138c3f34e638c421bfd8
Miraziz <khidoyatov1997@gmail.com>
2023-02-05 23:27:43 +0500

    Implement Customer details VM

---
+++ Inventory/Inventory/Modules/Inventory.Modules.Customers/ViewModels/Forms/CustomerDetailsFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Inventory.Core;
+using Inventory.Core.Mvvm;
+using MaterialDesignThemes.Wpf;
+using Prism.Commands;
+
+namespace Inventory.Modules.Customers.ViewModels.Forms
+{
+    public class CustomerDetailsFormViewModel : ViewModelBase
+    {
+        public DelegateCommand CancelCommand { get; }
+
+        public CustomerDetailsFormViewModel()
+        {
+            CancelCommand = new DelegateCommand(OnCancel);
+        }
+
+        private void OnCancel()
+        {
+            DialogHost.Close(RegionNames.DialogRegion);
+        }
+    }
+}
