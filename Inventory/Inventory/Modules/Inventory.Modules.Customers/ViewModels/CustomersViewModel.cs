c15f37b7429cfcaf85cb683579b6fa2d242e3ec4
Miraziz <khidoyatov1997@gmail.com>
2023-02-05 15:29:50 +0500

    wrap commands methods into try-catch & use busy indicator

--- Inventory/Inventory/Modules/Inventory.Modules.Customers/ViewModels/CustomersViewModel.cs
+++ Inventory/Inventory/Modules/Inventory.Modules.Customers/ViewModels/CustomersViewModel.cs
@@ -1,22 +1,32 @@
-﻿using Inventory.Core.Dialogs;
+﻿using Inventory.Core.Mvvm;
 using Inventory.Modules.Customers.ViewModels.Forms;
 using Inventory.Modules.Customers.Views.Forms;
 using Inventory.Services.Interfaces;
 using MaterialDesignThemes.Wpf;
 using Prism.Commands;
-using Prism.Mvvm;
 using Prism.Regions;
 using ReverseAnalytics.Domain.DTOs.CustomerPhoneDto;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace Inventory.Modules.Customers.ViewModels
 {
-    public class CustomersViewModel : BindableBase, IRegionMemberLifetime
+    public class CustomersViewModel : ViewModelBase, IRegionMemberLifetime
     {
+
+        #region Services
+
         private readonly ICustomerService _customerService;
+        private readonly IDialogService _dialogService;
+
+        #endregion
+
+        #region Properties
 
         private string _selectedCompany;
         public string SelectedCompany
@@ -25,21 +35,32 @@ namespace Inventory.Modules.Customers.ViewModels
             set => SetProperty(ref _selectedCompany, value);
         }
 
+        public bool KeepAlive => true;
+
+        #endregion
+
+        #region Commands
+
         public ICommand DetailsCommand { get; }
         public ICommand AddCommand { get; }
         public ICommand UpdateCommand { get; }
         public ICommand ArchiveCommand { get; }
         public ICommand DeleteCommand { get; }
 
+        #endregion
+
+        #region Collections
+
         private readonly List<CustomerDto> customers;
         public ObservableCollection<CustomerDto> FilteredCustomers { get; private set; }
         public ObservableCollection<string> Companies { get; private set; }
 
-        public bool KeepAlive => true;
+        #endregion
 
-        public CustomersViewModel(ICustomerService customerService)
+        public CustomersViewModel(ICustomerService customerService, IDialogService dialogService)
         {
             _customerService = customerService;
+            _dialogService = dialogService;
 
             DetailsCommand = new DelegateCommand<CustomerDto>(OnShowDetails);
             AddCommand = new DelegateCommand(OnAddCustomer);
@@ -54,22 +75,136 @@ namespace Inventory.Modules.Customers.ViewModels
             LoadCustomers();
         }
 
+        #region Main methods
+
         private async void LoadCustomers()
         {
-            var result = await _customerService.GetCustomersAsync();
-
-            if (result == null || !result.Any())
+            try
             {
-                return;
+                IsBusy = true;
+
+                var result = await _customerService.GetCustomersAsync();
+
+                if (result == null) return;
+
+                var companies = result.Select(x => x.CompanyName)
+                    .Distinct()
+                    .ToList();
+
+                customers.AddRange(result);
+                FilteredCustomers.AddRange(result);
+                Companies.AddRange(companies);
             }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await _dialogService.ShowError();
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
 
-            var companies = result.Select(x => x.CompanyName)
-                .Distinct()
-                .ToList();
+        #endregion
 
-            customers.AddRange(result);
-            FilteredCustomers.AddRange(result);
-            Companies.AddRange(companies);
+        #region Command methods
+
+        private async void OnAddCustomer()
+        {
+            try
+            {
+                var view = new CustomerForm()
+                {
+                    DataContext = new CustomerFormViewModel()
+                };
+
+                var result = await DialogHost.Show(view, "RootDialog");
+
+                if (result is null)
+                {
+                    return;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await _dialogService.ShowError();
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async void OnUpdateCustomer(CustomerDto selectedCustomer)
+        {
+            try
+            {
+                var view = new CustomerForm()
+                {
+                    DataContext = new CustomerFormViewModel(selectedCustomer)
+                };
+
+                var result = await DialogHost.Show(view, "RootDialog");
+
+                if (result is null)
+                {
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await _dialogService.ShowError();
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async void OnArchiveCustomer()
+        {
+            try
+            {
+                bool isConfirm = await _dialogService.ShowConfirmation();
+
+                if (!isConfirm) return;
+
+                // TODO implement archive call
+                await Task.Delay(1500);
+                await _dialogService.ShowSuccess();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await _dialogService.ShowError();
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async void OnDeleteCustomer(CustomerDto selectedCustomer)
+        {
+            try
+            {
+                bool isConfirm = await _dialogService.ShowConfirmation();
+
+                if (!isConfirm) return;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await _dialogService.ShowError();
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
 
         private async void OnShowDetails(CustomerDto customerDto)
@@ -82,46 +217,6 @@ namespace Inventory.Modules.Customers.ViewModels
             await DialogHost.Show(view, "RootDialog");
         }
 
-        private async void OnAddCustomer()
-        {
-            var view = new CustomerForm()
-            {
-                DataContext = new CustomerFormViewModel()
-            };
-
-            var result = await DialogHost.Show(view, "RootDialog");
-
-            if (result is null)
-            {
-                return;
-            }
-        }
-
-        private async void OnUpdateCustomer(CustomerDto selectedCustomer)
-        {
-            var view = new CustomerForm()
-            {
-                DataContext = new CustomerFormViewModel(selectedCustomer)
-            };
-
-            var result = await DialogHost.Show(view, "RootDialog");
-
-            if (result is null)
-            {
-                return;
-            }
-        }
-
-        private void OnArchiveCustomer()
-        {
-
-        }
-
-        private async void OnDeleteCustomer(CustomerDto selectedCustomer)
-        {
-            var view = new ConfirmationDialog("Confirm action", $"Are you sure you want to delete customer: {selectedCustomer.FullName}?");
-
-            var result = await DialogHost.Show(view, "RootDialog");
-        }
+        #endregion
     }
 }
