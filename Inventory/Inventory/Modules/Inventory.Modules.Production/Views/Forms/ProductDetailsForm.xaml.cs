9dac2661d3c067c36f419c27e3ba846040f080f8
Miraziz <khidoyatov1997@gmail.com>
2023-02-05 12:04:02 +0500

    Implement product details form

---
+++ Inventory/Inventory/Modules/Inventory.Modules.Production/Views/Forms/ProductDetailsForm.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Inventory.Modules.Production.ViewModels.Forms;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Inventory.Modules.Production.Views.Forms
+{
+    /// <summary>
+    /// Interaction logic for ProductDetailsForm.xaml
+    /// </summary>
+    public partial class ProductDetailsForm : UserControl
+    {
+        public ProductDetailsForm()
+        {
+            InitializeComponent();
+
+            PreviewKeyDown += new KeyEventHandler(HandleEsc);
+        }
+
+        private void HandleEsc(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                var vm = DataContext as CategoryDetailsFormViewModel;
+
+                vm?.OnClose();
+            }
+        }
+    }
+}
