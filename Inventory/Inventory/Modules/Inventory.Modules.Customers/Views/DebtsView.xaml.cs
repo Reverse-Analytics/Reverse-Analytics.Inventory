88e3019e0707f4e4c1b8737dbc6981b8d870f07b
Miraziz <khidoyatov1997@gmail.com>
2023-02-09 17:25:16 +0500

    Implement debts page

--- Inventory/Inventory/Modules/Inventory.Modules.Customers/Views/DebtsView.xaml.cs
+++ Inventory/Inventory/Modules/Inventory.Modules.Customers/Views/DebtsView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Inventory.Helpers.Extensions;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Inventory.Modules.Customers.Views
 {
@@ -23,6 +11,9 @@ namespace Inventory.Modules.Customers.Views
         public DebtsView()
         {
             InitializeComponent();
+
+            customersDataGrid.CellRenderers.Remove("TemplateExt");
+            customersDataGrid.CellRenderers.Add("TemplateExt", new GridCellTemplateExtension());
         }
     }
 }
