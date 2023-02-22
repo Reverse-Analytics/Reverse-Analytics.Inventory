9c545615e8a6f60f61d723c23797ca6e002b9595
Miraziz <khidoyatov1997@gmail.com>
2023-02-22 21:35:58 +0500

    Implement status to icon converter

---
+++ Inventory/Inventory/Inventory.Helpers/Converters/StatusToIconConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Inventory.Helpers.Converters
+{
+    public class StatusToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string status = value.ToString();
+
+            return Core.Resources.IconsPath.PaymentRequired;
+
+            if (status == "Closed")
+            {
+
+            }
+            else if (status == "Due Soon")
+            {
+
+            }
+            else if (status == "Overdue")
+            {
+
+            }
+            else if (status == "Payment Required")
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
