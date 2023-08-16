809c0a8744b2b11f9b10ada5a47110e7527604eb
Miraziz <khidoyatov1997@gmail.com>
2023-08-16 22:17:13 +0400

    Show dashboard on start

--- Inventory/Inventory/Modules/Inventory.Modules.Regions/RegionsModule.cs
+++ Inventory/Inventory/Modules/Inventory.Modules.Regions/RegionsModule.cs
@@ -1,4 +1,5 @@
 ﻿using Inventory.Core;
+using Inventory.Modules.Dashboard.Views;
 using Inventory.Modules.Regions.Views;
 using Prism.Ioc;
 using Prism.Modularity;
@@ -19,12 +20,14 @@ namespace Inventory.Modules.Regions
         {
             _regionManager.RequestNavigate(RegionNames.MenuRegion, "MenuView");
             _regionManager.RequestNavigate(RegionNames.NavigationRegion, "SidebarView");
+            _regionManager.RequestNavigate(RegionNames.ContentRegion, "DashboardView");
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterForNavigation<MenuView>();
             containerRegistry.RegisterForNavigation<SidebarView>();
+            containerRegistry.RegisterForNavigation<DashboardView>();
         }
     }
 }
