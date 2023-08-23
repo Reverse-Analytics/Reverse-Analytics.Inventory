1111c738fd342e73ed1053703cf52a1a027a9f4a
Miraziz <khidoyatov1997@gmail.com>
2023-08-23 23:57:56 +0400

    Create PhoneNumberValidationRule.cs

---
+++ Inventory/Inventory/Inventory.Core/ValidationRules/PhoneNumberValidationRule.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Inventory.Core.ValidationRules
+{
+    internal class PhoneNumberValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return IsPhoneNumberValid(value) ?
+                ValidationResult.ValidResult :
+                new ValidationResult(false, "Invalid phone format.");
+        }
+
+        private bool IsPhoneNumberValid(object value)
+        {
+            if (value is not string phoneNumber)
+            {
+                return false;
+            }
+
+            if (phoneNumber.Length != 14)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
