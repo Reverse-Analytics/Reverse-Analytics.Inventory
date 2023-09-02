61c73475a0b719a2a4987d19ca632e38b8271444
Miraziz <khidoyatov1997@gmail.com>
2023-09-02 21:14:30 +0500

    Update SaleDetail.cs

--- Inventory/Inventory/Inventory.Core/Models/SaleDetail.cs
+++ Inventory/Inventory/Inventory.Core/Models/SaleDetail.cs
@@ -6,6 +6,7 @@
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal Discount { get; set; }
+        public decimal TotalDue { get; set; }
 
         public int SaleId { get; set; }
         public virtual Sale Sale { get; set; }
